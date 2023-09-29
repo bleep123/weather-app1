5e518ad53f94daa2aca46cf4f479520952936d15
Shreya Sahai <125577329+bleep123@users.noreply.github.com>
2023-09-29 21:34:06 +0530

    Removed error in JS file

--- script.js
+++ script.js
@@ -10,27 +10,26 @@ let daysArray = [
   "Wednesday",
   "Thursday",
   "Friday",
-  "Saturday",
-  
+  "Saturday"
 ];
 
-let dateTime = document.querySelector("#date-time");
-dateTime.innerHTML = `29 degree celsius<br> ${daysArray[day]} ${hours}:${minutes}`;
+function display(response) {
+  document.querySelector("#city-name").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
 
-
-function display(response){
-  document.querySelector("#city-name").innerHTML=response.data.name;
-  document.querySelector("#temperature").innerHTML=Math.round(response.data.main.temp);
+  let dateTime = document.querySelector("#date-time");
+  if (minutes < 10) {
+    dateTime.innerHTML = ` ${daysArray[day]} ${hours}:0${minutes}`;
+  }
+    dateTime.innerHTML = ` ${daysArray[day]} ${hours}:${minutes}`;
 }
 
-
 function submitForm(event) {
-event.preventDefault();
-let city = document.querySelector("#city-name").value;
-let apiKey="5a662a43a108de0bc455889e9f2204f1";
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
-axios.get(`${apiUrl}&appid=${apiKey}`).then(display);
-console.log(apiUrl);
+  event.preventDefault();
+  let apiKey = "5a662a43a108de0bc455889e9f2204f1";
+  let city = document.querySelector("#name").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(display);
 }
 
 let search = document.querySelector("#search");
