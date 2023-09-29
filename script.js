296f39a6ba46e2d7b2cf925e6cced46d49221c74
Shreya Sahai <125577329+bleep123@users.noreply.github.com>
2023-09-29 22:51:11 +0530

    Added icon,wind,desc,unit

--- script.js
+++ script.js
@@ -14,14 +14,22 @@ let daysArray = [
 ];
 
 function display(response) {
+  console.log(response.data)
   document.querySelector("#city-name").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
+  document.querySelector("#weather-description").innerHTML = response.data.weather[0].description;
+  document.querySelector("#wind-speed").innerHTML = response.data.wind.speed;
+
+  celsiusTemperature = response.data.main.temp;
 
   let dateTime = document.querySelector("#date-time");
   if (minutes < 10) {
     dateTime.innerHTML = ` ${daysArray[day]} ${hours}:0${minutes}`;
   }
     dateTime.innerHTML = ` ${daysArray[day]} ${hours}:${minutes}`;
+
+  let iconElement= document.querySelector("#icon");
+  iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
 }
 
 function submitForm(event) {
@@ -32,5 +40,26 @@ function submitForm(event) {
   axios.get(apiUrl).then(display);
 }
 
+function displayFahrenheit(event) {
+  event.preventDefault();
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
 let search = document.querySelector("#search");
 search.addEventListener("submit", submitForm);
+
+let celsiusTemperature = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsius);
